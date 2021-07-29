46ff90410cae2e5937c7325bec9adc555564e416
Beast <vaibhavv96321@gmail.com>
2021-07-30 04:01:35 +0530

    Themes Added and pages seperated Extraction of Widgets and making class to use widgets on our own way

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -12,9 +13,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Containerforlayout(colour: Color(0xFF1F1E33)),
+                ),
+                Expanded(
+                  child: Containerforlayout(colour: Color(0xFF1F1E33)),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Containerforlayout(colour: Color(0xFF1F1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Containerforlayout(colour: Color(0xFF1F1E33)),
+                ),
+                Expanded(
+                  child: Containerforlayout(colour: Color(0xFF1F1E33)),
+                )
+              ],
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class Containerforlayout extends StatelessWidget {
+  Containerforlayout({this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration:
+          BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
     );
   }
 }
